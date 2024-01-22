3d9d1663dd8f22b21388057ea0f921e267adefaf
didinarara <dsakenova06@gmail.com>
2024-01-22 19:15:46 +0600

    Initial Commit

---
+++ pp10.py
@@ -0,0 +1,21 @@
+x = 5    #int
+y = 3.7  #float
+z = 3+5j #complex
+print(type(x))
+print(type(y))
+print(type(z))
+
+#convert from int to float
+a = float(x)
+#convert from float to int
+b = int(y)
+#convert from int to complex
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
