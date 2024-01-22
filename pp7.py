fae03774beebc975366ce2958f330817ee6f2d65
didinarara <dsakenova06@gmail.com>
2024-01-22 18:32:11 +0600

    Initial Commit

---
+++ pp7.py
@@ -0,0 +1,19 @@
+x = "I love KTL"
+print(x)
+#is the same
+a = "I "
+b = "love "
+c = "KTL"
+print(a, b, c)
+#is also the same
+print(a + b + c)
+
+'''
+but when we use numbers, 
++ works as a
+mathematical operator
+'''
+#for example
+y = z = 30
+print(y + z) 
+#hope to have this point in attestations
