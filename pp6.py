c6ad1a32e146a4dd06285062e474f505af5638bf
didinarara <dsakenova06@gmail.com>
2024-01-22 18:20:42 +0600

    Initial Commit

---
+++ pp6.py
@@ -0,0 +1,15 @@
+x, y, z = 5, 10, 15
+print(x)
+print(y)
+print(z)
+
+a = b = c = "KBTU"
+print(a)
+print(b)
+print(c)
+
+cars = ["BMW","Mercedes", "Porsche"]
+l, m, n = cars
+print(l)
+print(m)
+print(n)
